04b5d929d9766480674d06a375df99c2af5e42e8
Kuripipeer <mchristianaxel@gmail.com>
2026-05-26 19:10:00 -0600

    Initial Commit

---
+++ Stockcito-Front/Stockcito_FrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stockcito_FrontApp.swift
+//  Stockcito-Front
+//
+//  Created by Christian Moreno on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Stockcito_FrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
