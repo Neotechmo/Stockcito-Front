e11cbbc70d7de314cc85891f7ad7001beb8a9fcd
Sergio Ernesto Rosas Ducoing <rosasducoing@gmail.com>
2026-06-05 16:40:54 -0600

    Initial Commit

---
+++ Stockcito-Front/Stockcito_FrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stockcito_FrontApp.swift
+//  Stockcito-Front
+//
+//  Created by Sergio Ernesto Rosas Ducoing on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Stockcito_FrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
